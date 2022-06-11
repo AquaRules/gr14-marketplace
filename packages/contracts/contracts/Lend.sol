4fb8477552aac6e7cc3601ec97e2f562a2fc3bae
SurroundingArt64 <97761020+SurroundingArt64@users.noreply.github.com>
2022-06-11 13:09:26 +0530

    feat: mortgage function

--- packages/contracts/contracts/Lend.sol
+++ packages/contracts/contracts/Lend.sol
@@ -2,26 +2,31 @@
 pragma solidity 0.8.6;
 
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
+
 import "./AccessControl.sol";
 
 /// @title Lend
 /// @notice User can come and mortgage his NFT
-contract Lend is AccessControl {
+contract Lend is AccessControl, ReentrancyGuardUpgradeable {
 	struct TokenData {
 		uint256 price;
 		uint256 duration;
 		uint256 roi;
 	}
 
+	struct MortgageData {
+		uint256 dateOfMortgage;
+	}
+
 	/// @dev token price
-	mapping(IERC721 => TokenData) public tokenPrice;
+	mapping(IERC721 => TokenData) public tokenData;
 
 	/// @dev if a token is allowed to be staked
 	mapping(IERC721 => bool) public allowedToken;
 
-	struct MortgageData {
-		uint256 dateOfMortgage;
-	}
+	IERC20 public currency;
 
 	/// @dev user to token to tokenId to mortgage data;
 	mapping(address => mapping(IERC721 => mapping(uint256 => MortgageData))) _mortgageData;
@@ -42,23 +47,38 @@ contract Lend is AccessControl {
 
 	/// @dev Allows admin to token token data
 	/// @param _token nft token address
-	/// @param tokenData mortgage price
-	function setTokenPrice(IERC721 _token, TokenData memory tokenData)
+	/// @param _tokenData mortgage price
+	function setTokenData(IERC721 _token, TokenData memory _tokenData)
 		external
 		onlyAdmin
 	{
-		tokenPrice[_token] = tokenData;
-		emit SetTokenPrice(_token, tokenData);
+		tokenData[_token] = _tokenData;
+		emit SetTokenData(_token, _tokenData);
 	}
 
-	function getInterest(
-		uint256 p,
-		uint256 r,
-		uint256 t
-	) public view returns (uint256) {
-		return (decimals * (p * r * t)) / 100;
+	function mortgage(IERC721 _token, uint256 tokenId) external nonReentrant {
+		require(allowedToken[_token], "Token not allowed");
+		_token.transferFrom(_msgSender(), address(this), tokenId);
+
+		_mortgageData[_msgSender()][_token][tokenId] = MortgageData({
+			dateOfMortgage: block.timestamp
+		});
+
+		currency.transferFrom(
+			address(this),
+			_msgSender(),
+			tokenData[_token].price
+		);
 	}
+
+	// function getInterest(
+	// 	uint256 p,
+	// 	uint256 r,
+	// 	uint256 t
+	// ) public view returns (uint256) {
+	// 	return (decimals * ((p * r * t))) / 100;
+	// }
 
 	event SetAllowedToken(IERC721 indexed _token, bool enabled);
-	event SetTokenPrice(IERC721 indexed _token, TokenData price);
+	event SetTokenData(IERC721 indexed _token, TokenData price);
 }
