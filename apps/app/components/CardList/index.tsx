4a3775f6cb3d7c47216de2349671f12ba257aa15
AquaRules <aquarules22@gmail.com>
2022-06-11 13:30:08 +0530

    feat: css update

--- apps/app/components/CardList/index.tsx
+++ apps/app/components/CardList/index.tsx
@@ -1,10 +1,14 @@
+import Image from 'next/image';
 import React from 'react';
 import { Card } from '../Card';
 import styles from './index.module.scss';
 
 export const CardList: React.FC = () => {
   const [chainSelect, setChainSelect] = React.useState<number>(0);
-  const chainOptions = [{ title: 'ethereum' }, { title: 'polygon' }];
+  const chainOptions = [
+    { title: 'ethereum', url: '/eth.png', color: 'primary' },
+    { title: 'polygon', url: '/matic.png', color: 'secondary' },
+  ];
 
   return (
     <div className={styles.wrapper}>
@@ -13,10 +17,20 @@ export const CardList: React.FC = () => {
           return (
             <div
               key={i}
-              datatype={i == chainSelect ? 'active' : ''}
+              datatype={i == chainSelect ? 'active ' + v.color : v.color}
               className={styles.button}
               onClick={() => setChainSelect(i)}
             >
+              <div className={styles.image}>
+                <Image
+                  src={v.url}
+                  alt={v.title}
+                  width="100%"
+                  height="100%"
+                  layout="responsive"
+                  objectFit="contain"
+                />
+              </div>
               {v.title}
             </div>
           );
