00918c4d70fc8e49d419f71d5539b82008a16e86
AquaRules <aquarules22@gmail.com>
2022-06-19 15:36:59 +0530

    feat: add cards

--- apps/app/components/Card/index.tsx
+++ apps/app/components/Card/index.tsx
@@ -2,16 +2,66 @@ import Image from 'next/image';
 import React from 'react';
 import styles from './index.module.scss';
 
-export const Card: React.FC<{attributes:any}> = ({attributes}) => {
+export const Card: React.FC<{ attributes: any }> = ({ attributes }) => {
   return (
     <div className={styles.wrapper}>
+      <div className={styles.inner}>
+        <div className={styles.front}>
+          <Image
+            unoptimized
+            src={attributes.image_url}
+            alt="NFT Image"
+            layout="fill"
+            objectFit='cover'
+          />
+        </div>
+        <div className={styles.back}>
+          <h1>About</h1>
+          <ul>
+            <li>
+              <div>TYPE</div>
+              <div>{attributes.type}</div>
+            </li>
+            <li>
+              <div>ID</div>
+              <div>{attributes.id}</div>
+            </li>
+            <li>
+              <div>VALUE</div>
+              <div>
+                {attributes.price.amount} {attributes.price.chainName}
+              </div>
+            </li>
+            <li>
+              <div>LEASE</div>
+              <div>
+                {attributes.lend.amount} {attributes.price.chainName} / WEEK
+              </div>
+            </li>
+          </ul>
+          <h1>Author</h1>
+          <ul>
+            <li>
+              <div>NAME</div>
+              <div>{attributes.owner}</div>
+            </li>
+            <li>
+              <div>AGE</div>
+              <div>{new Date(attributes.created).getHours()} Hours Ago</div>
+            </li>
+            <li>
+              <div>LIKES</div>
+              <div>{attributes.likes}</div>
+            </li>
+          </ul>
+        </div>
+      </div>
       <div className={styles.hanging} datatype="left">
         @{attributes.owner}
       </div>
       <div className={styles.hanging} datatype="right">
         {attributes.likes} likes
       </div>
-      <Image unoptimized src={attributes.image_url} alt="NFT Image" layout='fill'/>
     </div>
   );
 };
--- apps/app/components/CardList/index.tsx
+++ apps/app/components/CardList/index.tsx
@@ -14,13 +14,62 @@ export const CardList: React.FC = () => {
     {
       title: 'Card#1',
       owner: 'abhinavr',
-      minted: Date.now() - 100 * 60 * 60 * 24,
+      created: Date.now() - 100 * 60 * 60 * 24,
       likes: 1243,
       type: 'Comedy',
       id: '1',
-      price: { chain: 1, amount: 2 },
+      price: { chain: 1, amount: 2, chainName: 'ETH' },
       lend: { enabled: true, amount: 0.1 },
-      image_url: "https://64.media.tumblr.com/780e42f226309d1998293cee15814051/tumblr_mubkq9dZhh1rmfvsio1_400.gif"
+      image_url:
+        'https://64.media.tumblr.com/780e42f226309d1998293cee15814051/tumblr_mubkq9dZhh1rmfvsio1_400.gif',
+    },
+    {
+      title: 'Card#1',
+      owner: 'abhinavr',
+      created: Date.now() - 100 * 60 * 60 * 24,
+      likes: 1243,
+      type: 'Comedy',
+      id: '1',
+      price: { chain: 1, amount: 2, chainName: 'ETH' },
+      lend: { enabled: true, amount: 0.1 },
+      image_url:
+        'https://media2.giphy.com/media/G3lxvBMhGu53y/giphy.gif',
+    },
+    {
+      title: 'Card#1',
+      owner: 'abhinavr',
+      created: Date.now() - 100 * 60 * 60 * 24,
+      likes: 1243,
+      type: 'Comedy',
+      id: '1',
+      price: { chain: 1, amount: 2, chainName: 'ETH' },
+      lend: { enabled: true, amount: 0.1 },
+      image_url:
+        'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmedia.giphy.com%2Fmedia%2F4GDliBrmcmuFa%2Fgiphy.gif&f=1&nofb=1',
+    },
+    {
+      title: 'Card#1',
+      owner: 'abhinavr',
+      created: Date.now() - 100 * 60 * 60 * 24,
+      likes: 1243,
+      type: 'Comedy',
+      id: '1',
+      price: { chain: 1, amount: 2, chainName: 'ETH' },
+      lend: { enabled: true, amount: 0.1 },
+      image_url:
+        'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmedia1.tenor.com%2Fimages%2F8832ed90df55d2f8a1aae2ff3e183c00%2Ftenor.gif%3Fitemid%3D9254331&f=1&nofb=1',
+    },
+    {
+      title: 'Card#1',
+      owner: 'abhinavr',
+      created: Date.now() - 100 * 60 * 60 * 24,
+      likes: 1243,
+      type: 'Comedy',
+      id: '1',
+      price: { chain: 1, amount: 2, chainName: 'ETH' },
+      lend: { enabled: true, amount: 0.1 },
+      image_url:
+        'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.lBsU-sg7q1Uxn9r9gtNrwgHaHW%26pid%3DApi&f=1',
     },
   ];
 
@@ -51,10 +100,9 @@ export const CardList: React.FC = () => {
         })}
       </div>
       <div className={styles.cardWrapper}>
-        {dummyCards
-          .map((v,i) => {
-            return <Card key={i} attributes={v}/>;
-          })}
+        {dummyCards.map((v, i) => {
+          return <Card key={i} attributes={v} />;
+        })}
       </div>
     </div>
   );
